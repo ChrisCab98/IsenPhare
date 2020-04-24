a622d40b686941bd0e5adb41059df0c995735555
christopher.cabantous <christopher.cabantous@devisubox.com>
2020-04-24 10:31:46 +0200

    Change name

--- app/src/main/java/isen/p16/isenphare/ui/gallery/GalleryFragment.java
+++ app/src/main/java/isen/p16/isenphare/ui/gallery/GalleryFragment.java
@@ -1,5 +1,6 @@
 package isen.p16.isenphare.ui.gallery;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,31 +9,50 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import isen.p16.isenphare.R;
 
-public class GalleryFragment extends Fragment {
-
+public class GalleryFragment extends Fragment implements OnMapReadyCallback  {
     private GalleryViewModel galleryViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.activity_maps, container, false);
-        /*
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-
-       */
         return root;
     }
+    private final static LatLng GARDANNE = new LatLng(43.455669,5.47064899999998);
+
+    @Override
+    public void onActivityCreated (Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        SupportMapFragment supportMapFragment = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map));
+        supportMapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(GARDANNE), 2000, null);
+        googleMap.addMarker(new MarkerOptions().position(GARDANNE));
+        if (ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        else
+            googleMap.setMyLocationEnabled(true);
+    }
+
 }
